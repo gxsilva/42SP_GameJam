0b123605ad6f505fe7ffb3cb0517b9f8551451d0
Caio De Souza Marinho <caide-so@c2r11p3.42sp.org.br>
2025-09-20 20:59:59 -0300

    player dead bug

--- marginalbits/scripts/goal.gd
+++ marginalbits/scripts/goal.gd
@@ -5,11 +5,7 @@ extends Area2D
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		if next_level:
-			print("Entering level: " + next_level.resource_path)
-			# Deferred scene change
 			call_deferred("change_scene", next_level)
-		else:
-			push_warning("Goal node has no next_level assigned!")
 
 # Helper function called deferred
 func change_scene(scene: PackedScene) -> void:
