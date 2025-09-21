37e5f0816251812c95cd432c2e681a9ea27b7e43
gxsilva <lssvgabriel@gmail.com>
2025-09-20 23:19:57 -0300

    fix killzone

--- marginalbits/scripts/killzone.gd
+++ marginalbits/scripts/killzone.gd
@@ -3,10 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("you died")
 		timer.start()
 
 func _on_timer_timeout() -> void:
-	print("reset")
 	get_tree().reload_current_scene()
